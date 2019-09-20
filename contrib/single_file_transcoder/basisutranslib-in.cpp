bd8feea6d9abce40171be1ae4ef66998fe7356c7
Carl Woffenden <cwoffenden@gmail.com>
2019-09-20 15:00:02 -0400

    Initial commit
    Added combine script.  Created initial Emscripten example.

---
+++ contrib/single_file_transcoder/basisutranslib-in.cpp
@@ -0,0 +1,12 @@
+/**
+ * Basis Universal single file library. Generated using:
+ * \code
+ *	./combine.sh -r ../../transcoder -o basisutranslib.cpp basisutranslib-in.cpp
+ * \endcode
+ * We can add any build options here, e.g.:
+ * \code
+ *	#define BASISD_SUPPORT_BC7 0
+ * \endcode
+ */
+
+#include "basisu_transcoder.cpp"
