db3197b3c52e3a022e14293a233d0b025f4f54bb
Donovan Hutchence <dhutchence@snapchat.com>
2020-11-16 14:27:55 +0000

    align with upstream

--- basisu_comp.cpp
+++ basisu_comp.cpp
@@ -72,6 +72,7 @@ namespace basisu
 			PRINT_BOOL_VALUE(m_compute_stats);
 			PRINT_BOOL_VALUE(m_check_for_alpha)
 			PRINT_BOOL_VALUE(m_force_alpha)
+			PRINT_BOOL_VALUE(m_renormalize);
 			PRINT_BOOL_VALUE(m_multithreading);
 			PRINT_BOOL_VALUE(m_disable_hierarchical_endpoint_codebooks);
 			
@@ -99,8 +100,6 @@ namespace basisu
 			debug_printf("m_tex_type: %u\n", m_params.m_tex_type);
 			debug_printf("m_userdata0: 0x%X, m_userdata1: 0x%X\n", m_params.m_userdata0, m_params.m_userdata1);
 			debug_printf("m_us_per_frame: %i (%f fps)\n", m_params.m_us_per_frame, m_params.m_us_per_frame ? 1.0f / (m_params.m_us_per_frame / 1000000.0f) : 0);
-
-			PRINT_BOOL_VALUE(m_renormalize);
 
 			debug_printf("swizzle: %d,%d,%d,%d\n",
 				m_params.m_swizzle[0],
@@ -405,11 +404,8 @@ namespace basisu
 				file_image = m_params.m_source_images[source_file_index];
 			}
 
-			// normalize source image
 			if (m_params.m_renormalize)
-			{
 				file_image.renormalize_normal_map();
-			}
 
 			bool alpha_swizzled = false;
 			if (m_params.m_swizzle[0] != 0 ||
