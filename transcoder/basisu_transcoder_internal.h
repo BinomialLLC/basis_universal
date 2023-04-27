ad9386a4a1cf2a248f7bbd45f543a7448db15267
Richard Geldreich <richgel99@protonmail.com>
2023-04-26 23:17:50 -0400

    Minor fix to two accessors so they return refs vs. copies.

--- transcoder/basisu_transcoder_internal.h
+++ transcoder/basisu_transcoder_internal.h
@@ -264,8 +264,8 @@ namespace basist
 		}
 
 		const basisu::uint8_vec &get_code_sizes() const { return m_code_sizes; }
-		const basisu::int_vec get_lookup() const { return m_lookup; }
-		const basisu::int16_vec get_tree() const { return m_tree; }
+		const basisu::int_vec &get_lookup() const { return m_lookup; }
+		const basisu::int16_vec &get_tree() const { return m_tree; }
 
 		bool is_valid() const { return m_code_sizes.size() > 0; }
 
