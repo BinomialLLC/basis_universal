f71fc47deca4f3c0f45ccdc552b7436f01c2ccb0
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2026-01-14 23:55:34 -0800

    Fix building on NetBSD

--- encoder/basisu_enc.h
+++ encoder/basisu_enc.h
@@ -4311,7 +4311,7 @@ namespace basisu
 	// Positive, negative, or denormals. No NaN or Inf. Clamped to MAX_HALF_FLOAT.
 	inline basist::half_float fast_float_to_half_trunc_no_nan_or_inf(float f)
 	{
-		assert(!isnan(f) && !isinf(f));
+		assert(!std::isnan(f) && !std::isinf(f));
 
 		// Sutract 112 from the exponent, to change the bias from 127 to 15.
 		static const fu32 g_f_to_h{ 0x7800000 };
@@ -4325,7 +4325,7 @@ namespace basisu
 
 	inline basist::half_float fast_float_to_half_trunc_no_clamp_neg_nan_or_inf(float f)
 	{
-		assert(!isnan(f) && !isinf(f));
+		assert(!std::isnan(f) && !std::isinf(f));
 		assert((f >= 0.0f) && (f <= basist::MAX_HALF_FLOAT));
 
 		// Sutract 112 from the exponent, to change the bias from 127 to 15.
@@ -4340,7 +4340,7 @@ namespace basisu
 
 	inline basist::half_float fast_float_to_half_no_clamp_neg_nan_or_inf(float f)
 	{
-		assert(!isnan(f) && !isinf(f));
+		assert(!std::isnan(f) && !std::isinf(f));
 		assert((f >= 0.0f) && (f <= basist::MAX_HALF_FLOAT));
 
 		// Sutract 112 from the exponent, to change the bias from 127 to 15.
