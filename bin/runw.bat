54bc84c3e4e6611784d40e749fa3beb1dba864d8
Richard Geldreich <richgel99@protonmail.com>
2026-01-22 18:28:40 -0500

    removing local test paths from WASM WASI run scripts

--- bin/runw.bat
+++ bin/runw.bat
@@ -2,4 +2,5 @@
 REM Example: "runw.bat test_images/xmen.png"
 REM Example: "runw.bat /bik/bik1.png"
 
-wasmtime --dir=. --dir=.. --dir=..\test_files --dir=d:/dev/test_images::/test_images --dir=d:/dev/test_images/bik::/bik basisu_st.wasm %*
+REM wasmtime --dir=. --dir=.. --dir=..\test_files --dir=d:/dev/test_images::/test_images --dir=d:/dev/test_images/bik::/bik basisu_st.wasm %*
+wasmtime --dir=. --dir=.. --dir=..\test_files basisu_st.wasm %*
--- bin/runwt.sh
+++ bin/runwt.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-wasmtime run --dir=. --dir=../test_files --dir=/mnt/d/dev/test_images::/test_images --dir=/mnt/d/dev/test_images/bik::/test_images/bik --wasm threads=yes --wasi threads=yes ./basisu_mt.wasm "$@"
+wasmtime run --dir=. --dir=../test_files --wasm threads=yes --wasi threads=yes ./basisu_mt.wasm "$@"
